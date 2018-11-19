3cdfe7650fa23e6c628b8a5fb324bea83d80ec19
supernelly <nelsonsu13@yahoo.ca>
2018-11-19 08:34:43 -0500

    e
    e

--- Assignment 3/Assignment 3/Program.cs
+++ Assignment 3/Assignment 3/Program.cs
@@ -19,26 +19,32 @@ namespace Assignment_3
             leftMostChild = left;
             rightSibling = right;
         }
-    }
+    }    
     public class FileSystem
     {
         private Node root;
         // Creates a file system with a root directory
         public FileSystem() { ... }
+
         // Adds a file at the given address
         // Returns false if the file already exists or the path is undefined; true otherwise
         public bool AddFile(string address) { ... }
+
         // Removes the file at the given address
         // Returns false if the file is not found or the path is undefined; true otherwise
         public bool RemoveFile(string address) { ... }
+
         // Adds a directory at the given address
         // Returns false if the directory already exists or the path is undefined; true otherwise
         public bool AddDirectory(string address) { ... }
+
         // Removes the directory (and its subdirectories) at the given address
         // Returns false if the directory is not found or the path is undefined; true otherwise
         public bool RemoveDirectory(string address) { ... }
+
         // Returns the number of files in the file system
         public int NumberFiles() { ... }
+
         // Prints the directories in a pre-order fashion along with their files
         public void PrintFileSystem() { ... }
     }
